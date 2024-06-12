e84a6a669dbc8f6f8e3c7306fea1f0e5a0e326cd
cyvk <2730962665@qq.com>
2024-06-12 15:28:10 +0800

    init

--- cli/http/merge.go
+++ cli/http/merge.go
@@ -6,7 +6,7 @@ import (
 	"github.com/kaliwin/Needle/PublicStandard/ObjectHandling"
 	"github.com/kaliwin/ObjectConversion/process/HttpRawByteStreamList"
 	"github.com/spf13/cobra"
-	"log"
+	"io"
 	"os"
 )
 
@@ -20,15 +20,18 @@ var MergeCmd = &cobra.Command{
 			return
 		}
 
-		Merge(RawPath, output, size)
+		err := Merge(RawPath, output, size)
+		if err != nil {
+			cmd.Println(err)
+		}
 	},
 }
 
 // Merge 合并http数据
-func Merge(RawPath string, output string, size int) {
+func Merge(RawPath string, output string, size int) error {
 
 	if _, err := os.ReadDir(output); err != nil {
-		log.Fatalln(err)
+		return err
 	}
 
 	list := HttpRawByteStreamList.BurpFlowToHttpRawByteStreamList{
@@ -37,20 +40,23 @@ func Merge(RawPath string, output string, size int) {
 
 	stream, err := ObjectHandling.BuildFIleObjectStream(RawPath, true, Interface.ObjectTypeHttpGroupProto)
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
 
 	for {
 		next, err := stream.Next()
 		if err != nil {
-			break
+			if err == io.EOF {
+				break
+			}
+			return err
 		}
 
 		if http, ok := next.(*HttpStructureStandard.HttpReqAndRes); ok {
 
 			err := list.WriteFile(http)
 			if err != nil {
-				log.Fatalln(err)
+				return err
 			}
 		}
 	}
@@ -58,6 +64,7 @@ func Merge(RawPath string, output string, size int) {
 	list.TmpSize = 2
 	list.MaxSize = 0
 	_ = list.WriteFile(nil)
+	return err
 }
 
 var size int // 大小 单位MB
